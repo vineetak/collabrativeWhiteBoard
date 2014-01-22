a55f56018ec0fb49627029e9cc5bdeb538a51cf1
Vineeta Khatuja <vineetak@Vineetas-MacBook-Pro.local>
2014-01-22 11:52:09 -0500

    Adding loginActivity files

---
+++ src/com/example/drawingfun/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.drawingfun;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		System.out.println("Creating the login  activity");
+		super.onCreate(savedInstanceState);
+		// set the layout of the activity
+		setContentView(R.layout.login);
+		
+		// get the reference of the button
+		Button btnSignIn=(Button)findViewById(R.id.buttonSignIn);
+		final EditText editTextUserName=(EditText)findViewById(R.id.editTextUserNameToLogin);
+
+		// create a new handler to listen to the click on this button
+		View.OnClickListener loginHandler = new View.OnClickListener() {
+		    public void onClick(View v) {
+                /// Create Intent for drawView activity and abd Start The Activity
+		    	// TODO change the name of this MainActivity
+		    	
+                Intent intentDrawView =new Intent(getApplicationContext(),MainActivity.class);
+                
+                String userName=editTextUserName.getText().toString();
+                intentDrawView.putExtra("userID", userName);
+                startActivity(intentDrawView);
+		    }
+		 };
+		 
+		btnSignIn.setOnClickListener(loginHandler);
+            
+
+	}
+}
